891a1472fd0253fe2daf9620fd9475d94cac38b9
Hisokula <poymanova2004@mail.ru>
2024-05-03 22:40:03 +0200

    editing now availaible

--- lab5/all/code/save.php
+++ lab5/all/code/save.php
@@ -1,26 +1,49 @@
 <?php
+require_once "vendor/autoload.php";
 
-function redirectToHome(): void
-{
-    header('Location: /');
+use Google\Client;
+use Google\Service\Drive;
+use Google\Service\Sheets\SpreadSheet;
+
+if (empty($_POST['email'])) {
+    header("HTTP/1.1 404 Not Found");
     exit();
 }
+$inputData = array();
+$inputData[] = !empty($_POST['category']) ? $_POST['category'] : "other";
+$inputData[] = $_POST['email'];
+$inputData[] = !empty($_POST['title']) ? $_POST['title'] : "untitled";
+$inputData[] = $_POST['description'];
 
-if (false === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['description']))
-{
-    header('Location: /');
-    exit();
+$apiKey = "AIzaSyCyYRO0DaSwd1RF8m-pp4dJ13y_IfD4uew";
+$clientId = "***";
+$clientSecret = "***";
+
+$client = new Google_Client();
+$client->setApplicationName("testApplicationName");
+$client->setScopes([Google_Service_Sheets::SPREADSHEETS]);
+$client->setAccessType("offline");
+$client->setAuthConfig(__DIR__ . "/weblab5-422214-94f4bc6aa30b.json");
+$client->useApplicationDefaultCredentials();
+$client->addScope('https://www.googleapis.com/auth/spreadsheets');
+
+$service = new Google_Service_Sheets($client);
+$spreadsheetId = '1ucB_lakmSr89S1LfebLpfGjoW8AS3r8aU1DKqpB3tBI';
+$range = "sheet";
+
+$values = [[$inputData[0], $inputData[2], $inputData[3], $inputData[1]]];
+try {
+    $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+    $lastRowNumber = sizeof($response->getValues());
+
+    $body = new Google_Service_Sheets_ValueRange(['values' => $values]);
+    $options = array('valueInputOption' => 'RAW');
+
+    $service->spreadsheets_values->update($spreadsheetId, 'sheet!A' . ($lastRowNumber + 1), $body, $options);
+} catch (\Google\Service\Exception $e) {
+    echo "Возникла ошибка с получением данных";
 }
 
-$category = $_POST['category'];
-$title = $_POST['title'];
-$desc = $_POST['description'];
 
-$filePath = "categories/{$category}/{$title}.txt";
-
-if (false === file_put_contents($filePath, $desc))
-{
-    throw new Exception('Something went wrong!');
-}
-chmod($filePath, 0777);
-redirectToHome();
+header('Location: /');
+exit();
